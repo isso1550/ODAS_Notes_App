e32dddeae7cd49a346bdf64c1cc59cd82e693918
Filip Horst <jafiliph@gmail.com>
2022-11-26 20:57:10 +0100

    prosta rejestracja, jinja template

--- app/notes_app.py
+++ app/notes_app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, make_response
+import sqlite3
+import tools
+
+DB_FILE = "./notesapp.db"
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def welcome():
@@ -13,11 +18,19 @@ def register():
 @app.route("/register", methods = ['POST'])
 def registerUser():
     username = request.form.get("username")
-    email = request.form.get("email")
     password = request.form.get("password")
     confirmation = request.form.get("confirm-password")
+
     if password != confirmation:
         return "Passwords do not match!", 409
+    #!!!!!!!!!!!!!!!!!!!!!!!!TODO co jesli user w bazie?!!!!!!!!!!!!!!!!!!!!!
+    #!!!!!!!!!!!!!!!!!!!!!!!!TODO entropia hasła!!!!!!!!!!!!!!!!!!!
+    #!!!!!!!!!!!!!!!!!!!!!!!!TODO sprawdzanie czy uzytkownik probowal sql injection!!!!!!!!!!!!!!!!!!!!!
+    hash = tools.registerHash(password)
+    db = sqlite3.connect(DB_FILE)
+    sql = db.cursor()
+    sql.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {"username":username, "password": hash})
+    db.commit()
     return "Not implemented yet"
 
 @app.route("/login")
@@ -27,8 +40,35 @@ def login():
 def loginUser():
     username = request.form.get("username")
     password = request.form.get("password")
+    db = sqlite3.connect(DB_FILE)
+    sql = db.cursor()
+    db_hash = sql.execute("SELECT username, password FROM users WHERE username = :username", {"username":username}).fetchone()[1]
+    hash = tools.loginHash(password, db_hash)
+    if (hash == db_hash):
+        #LOGIN
+        return "Logged in as " + username
     return "Not implemented yet"
 
 @app.route("/browse")
 def browse():
     return "Not implemented yet"
+
+
+if __name__ == "__main__":
+    print("[*] Init database!")
+    db = sqlite3.connect(DB_FILE)
+    sql = db.cursor()
+    sql.execute("DROP TABLE IF EXISTS users;")
+    sql.execute("CREATE TABLE users (username varchar(100), password varchar(128));")
+    sql.execute("DELETE FROM users;")
+    #sql.execute("INSERT INTO users (username, password) VALUES ('bach', '$5$rounds=535000$ZJ4umOqZwQkWULPh$LwyaABcGgVyOvJwualNZ5/qM4XcxxPpkm9TKh4Zm4w4');")
+    #sql.execute("INSERT INTO users (username, password) VALUES ('john', '$5$rounds=535000$AO6WA6YC49CefLFE$dsxygCJDnLn5QNH/V8OBr1/aEjj22ls5zel8gUh4fw9');")
+    #sql.execute("INSERT INTO users (username, password) VALUES ('bob', '$5$rounds=535000$.ROSR8G85oGIbzaj$u653w8l1TjlIj4nQkkt3sMYRF7NAhUJ/ZMTdSPyH737');")
+
+    sql.execute("DROP TABLE IF EXISTS notes;")
+    sql.execute("CREATE TABLE notes (id INT PRIMARY KEY, username nvarchar(100), note nvarchar(256), privacy nvarchar(10));")
+    sql.execute("DELETE FROM notes;")
+    sql.execute("INSERT INTO notes (username, note, id) VALUES ('bob', 'To jest sekret!', 1);")
+    db.commit()
+
+    app.run("0.0.0.0", 5000)
---
+++ app/tools.py
@@ -0,0 +1,36 @@
+from time import sleep
+from passlib.hash import sha256_crypt
+
+#wielokrotne hashowanie - ile razy hashować?
+MULTIHASH_COUNT = 3
+#hashowanie - ile czasu odczekać[s]
+HASH_DELAY = 0.5
+
+
+#   Przetwarzanie hasła na hash
+#   Użyta funkcja jednokierunkowa: sha256 z pakietu passlib.hash
+def registerHash(password):
+    #pierwszy hash
+    hash = sha256_crypt.hash(password, rounds=535000)
+    #wykorzystanie soli wygenerowanej losowo przez funkcję sha256_crypt
+    salt = hash.split("$")[3]
+    #wielokrotne hashowanie
+    for i in range(1,MULTIHASH_COUNT):
+        hash = sha256_crypt.hash(hash, rounds=535000, salt=salt)
+    return hash
+
+def loginHash(password, db_hash):
+    salt = db_hash.split("$")[3]
+    #pierwszy hash
+    hash = sha256_crypt.hash(password, rounds=535000, salt=salt)
+    #wielokrotne hashowanie
+    for i in range(1,MULTIHASH_COUNT):
+        hash = sha256_crypt.hash(hash, rounds=535000, salt=salt)
+    #sztuczne opoznienie
+    sleep(HASH_DELAY)
+    return hash
+
+
+if __name__ == "__main__":
+    print("###TEST MODE###")
+    print(type(HASH_DELAY))
