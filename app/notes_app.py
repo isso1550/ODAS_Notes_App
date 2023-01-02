29d68e1239a8ab6c73424e278923638dba1eb74b
Filip Horst <jafiliph@gmail.com>
2023-01-02 15:55:01 +0100

    user avatar + basic note picture

--- app/notes_app.py
+++ app/notes_app.py
@@ -1,6 +1,10 @@
 from datetime import datetime, timedelta
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_file
 from flask_login import LoginManager, login_user, logout_user, login_required, current_user, UserMixin
+import os
+import glob
+import requests
+import filetype
 import sqlite3
 import tools
 import markdown
@@ -11,8 +15,15 @@ DB_FILE = "./notesapp.db"
 MAX_LOGIN_ATTEMPTS = 5
 ACCOUNT_SUSPENSION_TIME = timedelta(minutes=15)
 
+TEMP_SAVE_FOLDER = "./user_pictures/temp"
+AVATAR_SAVE_FOLDER = "./user_pictures/avatars"
+NOTEPIC_SAVE_FOLDER = "./user_pictures/notepics"
+FILE_ALLOWED_EXTENSIONS = ['png','jpg','jpeg','gif']
+FILE_MAX_SIZE = 8* 1024 * 1024
+
 login_manager = LoginManager()
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 8 * 1024 * 1024 #8mb
 app.secret_key = "abdabdabababababasbasbsabs"
 login_manager.init_app(app)
 #login_manager.login_view = 'login'
@@ -129,9 +140,11 @@ def saveNewNote():
     username = current_user.id
     #!!!!!!!!!!!!!!!!!!!!!!!!TODO GET USERNAME!!!!!!!!!!!!!!!!!!!!!!
     title = request.form.get("title")
+    picture_url = request.form.get('picture')
     privacy = request.form.get("privacy")
     note = request.form.get("note")
     
+    #privacy
     if(privacy not in ["private","unlisted","public"]):
         print(privacy)
         #!!!!!!!!!!!!!!!!!!!!!!!!!TODO log suspicious action!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -140,14 +153,47 @@ def saveNewNote():
         title = notecrypt.encrypt_note(title)
         note = notecrypt.encrypt_note(note)
         print("Remember to crypt!")
+    #title
     if(title == ""):
         title = "Untitled"
+    
     db = sqlite3.connect(DB_FILE)
     sql = db.cursor()
     #!!!!!!!!!!!!!!!!!!!TODO sprawdzenie czy uzytkownik probowal sqlnjection!!!!!!!!!!!!!!!!!!!!
     sql.execute("INSERT INTO notes (username, title, privacy, note) VALUES (:username, :title, :privacy, :note)", {"username":username, "title": title, "privacy": privacy, "note": note})
     db.commit()
     recent_id = sql.lastrowid
+
+    #pic
+    #!!!!!!!!!!!!!!!!!!!!!!!TODO upewnic sie testami ze jest ok !!!!!!!!!!!!!!!!!!!!!!!!
+    try:
+        response = requests.get(picture_url)
+        if (len(response.content) > FILE_MAX_SIZE):
+            return redirect("/render/" + str(recent_id))
+
+        temp_filepath = TEMP_SAVE_FOLDER + str(recent_id) + ".png"
+        open(temp_filepath, "wb").write(response.content)
+        kind = filetype.guess(temp_filepath)
+        if kind is None:
+            #_________________________________________________________LOG SUSPICIOUS
+            return "File invalid", 400
+        os.remove(temp_filepath)
+        if kind.extension in FILE_ALLOWED_EXTENSIONS:
+            #Usuwa stare (zeby nie bylo wielu zdjec z roznymi rozszerzeniami dla jednego uzytkownika)
+            pics = glob.glob(NOTEPIC_SAVE_FOLDER + "/" + str(recent_id) + ".*")
+            for pic in pics:
+                os.remove(pic)
+            filename = "/"+ str(recent_id) + "." + str(kind.extension)
+            filepath = os.path.join(NOTEPIC_SAVE_FOLDER + filename)
+            open(filepath, 'wb').write(response.content)
+            
+        else:
+            return "Unallowed filetype", 400
+    except Exception as e:
+        print(e)
+        return "Incorrect URL", 400
+
+
     return redirect("/render/" + str(recent_id))
 
 @app.route("/render/<id>")
@@ -231,15 +277,91 @@ def saveNewPassword():
     db.commit()
     return "Success"
 
+@app.route("/me")
+@login_required
+def myData():
+    username = current_user.id
+    return render_template("me.html", username=username)
+
+@app.route("/updateAvatar", methods=['POST'])
+@login_required
+def updateAvatar():
+    if 'file' not in request.files:
+        return redirect("/me")
+    file = request.files['file']
+    if file.filename == "":
+        return redirect("/me")
+    db = sqlite3.connect(DB_FILE)
+    sql = db.cursor()
+    try:
+        id = sql.execute("SELECT id FROM users WHERE username=:username", {"username": current_user.id}).fetchone()[0]
+        temp_filepath = os.path.join(TEMP_SAVE_FOLDER  + str(id)+".png")
+        file.save(temp_filepath)
+        kind = filetype.guess(temp_filepath)
+        if kind is None:
+            #_________________________________________________________LOG SUSPICIOUS
+            return "File invalid", 400
+        os.remove(temp_filepath)
+        if kind.extension in FILE_ALLOWED_EXTENSIONS:
+            #Usuwa stare (zeby nie bylo wielu zdjec z roznymi rozszerzeniami dla jednego uzytkownika)
+            pics = glob.glob(AVATAR_SAVE_FOLDER + "/" + str(id) + ".*")
+            for pic in pics:
+                os.remove(pic)
+            filename = "/"+ str(id) + "." + str(kind.extension)
+            filepath = os.path.join(AVATAR_SAVE_FOLDER + filename)
+            file.stream.seek(0)
+            file.save(os.path.join(filepath))
+        else:
+            return "Unallowed filetype", 400
+
+    except:
+        return redirect("/me")
+    return redirect("/me")
+
+@app.route("/getAvatar")
+@login_required
+def getAvatar():
+    db = sqlite3.connect(DB_FILE)
+    sql = db.cursor()
+    try:
+        id = sql.execute("SELECT id FROM users WHERE username=:username", {"username": current_user.id}).fetchone()[0]
+        pics = glob.glob(AVATAR_SAVE_FOLDER + "/" + str(id) + ".*")
+        if len(pics) > 0:
+            return send_file(pics[0])
+        else: return send_file(AVATAR_SAVE_FOLDER + "/default.png")
+    except Exception as e:
+        print(e)
+        return send_file(AVATAR_SAVE_FOLDER + "/default.png")
+
+@app.route("/getNotePicture/<id>")
+def getNotePicture(id):
+    db = sqlite3.connect(DB_FILE)
+    sql = db.cursor()
+    try:
+        author, privacy= sql.execute("SELECT username, privacy FROM notes WHERE id = :id", {"id":id}).fetchone()
+    except:
+        return redirect("/")
+    if (privacy == "private"):
+        if not (current_user.is_authenticated):
+            return "Login to browse private notes!"
+        username = current_user.id
+        if (username != author):
+            return "You are not the author!"
+        
+    pics = glob.glob(NOTEPIC_SAVE_FOLDER + "/" + str(id) + ".*")
+    if len(pics) > 0:
+        return send_file(pics[0])
+    return send_file(NOTEPIC_SAVE_FOLDER + "/default.png")
 
 if __name__ == "__main__":
     INIT_DB = False
+    INIT_USER_FILES = False
     if (INIT_DB):
         print("[*] Init database!")
         db = sqlite3.connect(DB_FILE)
         sql = db.cursor()
         sql.execute("DROP TABLE IF EXISTS users;")
-        sql.execute("CREATE TABLE users (email varchar(100), username varchar(100), password varchar(128));")
+        sql.execute("CREATE TABLE users (id INTEGER PRIMARY KEY, email varchar(100), username varchar(100), password varchar(128));")
         sql.execute("DELETE FROM users;")
 
         sql.execute("DROP TABLE IF EXISTS login_attempts;")
@@ -252,4 +374,14 @@ if __name__ == "__main__":
         sql.execute("INSERT INTO notes (username, note, id) VALUES ('bob', 'To jest sekret!', 1);")
         db.commit()
 
+    if (INIT_USER_FILES):
+        print("[*] Init user files! Deleting all except defaults...")
+        all = glob.glob(AVATAR_SAVE_FOLDER + "/*")
+        all.remove(glob.glob(AVATAR_SAVE_FOLDER + "/default*")[0])
+        for file in all:
+            os.remove(file)
+        all = glob.glob(NOTEPIC_SAVE_FOLDER + "/*")
+        all.remove(glob.glob(NOTEPIC_SAVE_FOLDER + "/default*")[0])
+        for file in all:
+            os.remove(file)
     app.run("0.0.0.0", 5000)
